4fc9561af5a5ae9c0922f5a5f8a9560204b36a91
gitwyrm <121982991+gitwyrm@users.noreply.github.com>
2025-03-17 16:59:47 +0100

    Adds more token sets

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
 import Quiz from "./components/Quiz";
 import "./App.css";
-import hiragana from "./hiragana";
-import katakana from "./katakana";
+import hiragana from "./tokens/hiragana";
+import katakana from "./tokens/katakana";
 
 function App() {
   return (
-    
-      <div className="side-by-side">
-        <div className="card">
-          <h1>Hiragana</h1>
-          <Quiz tokens={hiragana} />
-        </div>
-        <div style={{ width: "20px" }}></div>
-        <div className="card">
-          <h1>Katakana</h1>
-          <Quiz tokens={katakana} />
-        </div>
+    <div className="side-by-side">
+      <div className="card">
+        <h1>Hiragana</h1>
+        <Quiz tokens={hiragana} />
       </div>
-    
+      <div style={{ width: "20px" }}></div>
+      <div className="card">
+        <h1>Katakana</h1>
+        <Quiz tokens={katakana} />
+      </div>
+    </div>
   );
 }
 
