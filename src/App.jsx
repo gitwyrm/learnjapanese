efb3f2ee66b5233df28fe6156a27e0c3f94c8e5f
gitwyrm <121982991+gitwyrm@users.noreply.github.com>
2025-03-17 12:31:10 +0100

    Add Katakana support and enhance layout for quizzes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import Quiz from "./components/Quiz";
 import "./App.css";
 import hiragana from "./hiragana";
+import katakana from "./katakana";
 
 function App() {
   return (
-    <div className="card">
-      <Quiz tokens={hiragana} />
-    </div>
+    
+      <div className="side-by-side">
+        <div className="card">
+          <h1>Hiragana</h1>
+          <Quiz tokens={hiragana} />
+        </div>
+        <div style={{ width: "20px" }}></div>
+        <div className="card">
+          <h1>Katakana</h1>
+          <Quiz tokens={katakana} />
+        </div>
+      </div>
+    
   );
 }
 
